88fd301190fdaed411725359778a5afb378fa5ca
EliLentz <71344921+EliLentz@users.noreply.github.com>
2021-03-24 22:26:16 +0200

    Add files via upload

---
+++ Bunny.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bunnys_hill
+{
+    class Bunny
+    {
+        private enum Sex { male, female }
+        private enum Color { white, black, brown, spotted }//list of types of a bunny's color
+
+        private Sex m_pSex = new Sex();
+        private Color m_pColor = new Color();
+        private int m_pAge;
+        private string m_pName;
+
+        public Bunny(int m_pSex, int m_pColor, int m_pAge, string m_pName) //overloaded constructor
+                                                           //if custom information is there
+        {
+            this.m_pSex = (Bunny.Sex)m_pSex;
+            this.m_pColor = (Bunny.Color)m_pColor;
+            this.m_pAge = m_pAge;
+            this.m_pName = m_pName;
+
+            return;
+        }
+    }
+}
---
+++ Program_1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bunnys_hill
+{
+    class Program_1
+    {
+        private const int m_pBunnysQuan = 5; //how many bunnys we will create
+        private const int m_pBunnysChar = 3; //sex, color, age
+
+        private static int m_pSex;
+        private static int m_pColor;
+        private static int m_pAge;
+        private static string m_pName;
+
+        public static void generateBunny()
+        {
+            Random random = new Random();
+
+            int m_pSexMax = 2; //male and female
+            int m_pColorMax = 4; //white, black, brown, spotted
+            int m_pAgeMax = 11; //0 - 10
+
+            m_pSex = random.Next(m_pSexMax);
+            m_pColor = random.Next(m_pColorMax);
+            m_pAge = random.Next(m_pAgeMax);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Bunny[] bunny_arr = new Bunny[m_pBunnysQuan];
+
+            for (int i = 0; i < m_pBunnysQuan; i++)
+            {
+                Console.Write("Please, enter bunny's name: ");
+                m_pName = Console.ReadLine();
+                generateBunny();
+                
+                bunny_arr[i] = new Bunny(m_pSex, m_pColor, m_pAge, m_pName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
